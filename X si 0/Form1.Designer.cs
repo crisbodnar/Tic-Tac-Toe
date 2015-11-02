7d8fd6ac7eed8c61ca22d914d4a55b597e674e07
Cristian Bodnar <crisbodnar96@gmail.com>
2015-11-02 16:47:56 +0000

    Adding files

---
+++ X si 0/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace X_si_0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button4
+            // 
+            this.button4.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.button4.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.button4.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.button4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.button4.Location = new System.Drawing.Point(-9, 97);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(137, 111);
+            this.button4.TabIndex = 3;
+            this.button4.TabStop = false;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // button5
+            // 
+            this.button5.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.BackColor = System.Drawing.Color.White;
+            this.button5.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.button5.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.button5.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.button5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.button5.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.button5.Location = new System.Drawing.Point(123, 97);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(124, 101);
+            this.button5.TabIndex = 4;
+            this.button5.TabStop = false;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click_1);
+            // 
+            // button6
+            // 
+            this.button6.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.button6.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.button6.BackColor = System.Drawing.Color.White;
+            this.button6.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.button6.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.button6.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.button6.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.button6.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.button6.Location = new System.Drawing.Point(238, 97);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(135, 111);
+            this.button6.TabIndex = 5;
+            this.button6.TabStop = false;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click_1);
+            // 
+            // button7
+            // 
+            this.button7.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.button7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button7.BackColor = System.Drawing.Color.White;
+            this.button7.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.button7.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.button7.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.button7.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.button7.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.button7.Location = new System.Drawing.Point(-9, 195);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(137, 111);
+            this.button7.TabIndex = 6;
+            this.button7.TabStop = false;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click_1);
+            // 
+            // button8
+            // 
+            this.button8.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.button8.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button8.BackColor = System.Drawing.Color.White;
+            this.button8.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.button8.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.button8.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.button8.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.button8.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.button8.Location = new System.Drawing.Point(123, 195);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(124, 111);
+            this.button8.TabIndex = 7;
+            this.button8.TabStop = false;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click_1);
+            // 
+            // button9
+            // 
+            this.button9.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.button9.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button9.BackColor = System.Drawing.Color.White;
+            this.button9.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.button9.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.button9.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.button9.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.button9.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.button9.Location = new System.Drawing.Point(238, 195);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(135, 111);
+            this.button9.TabIndex = 8;
+            this.button9.TabStop = false;
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.button1.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.button1.Location = new System.Drawing.Point(-14, -2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 111);
+            this.button1.TabIndex = 9;
+            this.button1.TabStop = false;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.button2.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.button2.Location = new System.Drawing.Point(123, -2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 111);
+            this.button2.TabIndex = 10;
+            this.button2.TabStop = false;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.button3.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.button3.Location = new System.Drawing.Point(238, -2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 111);
+            this.button3.TabIndex = 11;
+            this.button3.TabStop = false;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 300);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(366, 339);
+            this.MinimumSize = new System.Drawing.Size(366, 339);
+            this.Name = "Form1";
+            this.Text = "X si 0";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+
+    }
+}
+
---
+++ X si 0/Form1.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace X_si_0
+{
+    public partial class Form1 : Form
+    {
+        int mutare = -1;
+        int[,] mat = new int[11, 11];
+        int[] line = new int[10];
+
+        const int CMAX = 100000;
+
+        static int[] C = new int[CMAX];
+        static int[] pow3 = new int[11];
+        static int[,] A = new int[4, 4];
+        static bool[] viz = new bool[CMAX];
+
+        public Form1()
+        {
+            InitializeComponent();
+            pow3[0] = 1;
+            for (int i = 1; i <= 10; i++)
+                pow3[i] = pow3[i - 1] * 3;
+            Back(1, 0);
+        }
+
+        static private int END_GAME()
+        {
+            if(A[0,0] == 1 && A[1,1] == 1 && A[2,2] == 1) return 1;
+            if(A[2,0] == 1 && A[1,1] == 1 && A[0,2] == 1) return 1;
+            if(A[0,0] == 2 && A[1,1] == 2 && A[2,2] == 2) return 1;
+            if(A[2,0] == 2 && A[1,1] == 2 && A[0,2] == 2) return 1;
+ 
+            for(int i = 0; i < 3; i++)
+            {
+                if(A[i,0] == 1 && A[i,1] == 1 && A[i,2] == 1) return 1;
+                if(A[0,i] == 1 && A[1,i] == 1 && A[2,i] == 1) return 1;
+                if(A[i,0] == 2 && A[i,1] == 2 && A[i,2] == 2) return 1;
+                if(A[0,i] == 2 && A[1,i] == 2 && A[2,i] == 2) return 1;
+            }
+            return 0;
+        }
+
+        static private int min(int a, int b)
+        {
+            if (a < b) return a;
+            return b;
+        }
+
+        static private int max(int a, int b)
+        {
+            if (a > b) return a;
+            return b;
+        }
+
+        static private void Back(int mov, int conf)
+        {
+            viz[conf] = true;
+ 
+            if(mov % 2 != 0)
+                C[conf] = 1; // X pierde
+            else
+                C[conf] = 3; // X castiga
+            if(END_GAME() != 0) return;
+ 
+            if(mov == 10)
+            {
+                C[conf] = 2; // egal
+                return;
+            }
+ 
+            for(int i = 0; i < 3; i++)
+                for(int j = 0; j < 3; j++)
+                    if(A[i,j] == 0)
+                        if(mov % 2 != 0)
+                        {
+                            A[i,j] = 1;
+                            int newconf = conf + pow3[3 * i + j];
+                            if(!viz[newconf])
+                                Back(mov + 1, newconf);
+                            C[conf] = max(C[conf], C[newconf]);
+                            A[i,j] = 0;
+                        }
+                        else
+                        {
+                            A[i,j] = 2;
+                            int newconf = conf + 2 * pow3[3 * i + j];
+                            if(!viz[newconf])
+                                Back(mov + 1, newconf);
+                            C[conf] = min(C[conf], C[newconf]);
+                            A[i,j] = 0;
+                        }
+        }
+
+        private static int Jucator(int mut)
+        {
+            if (mut % 2 == 1) return 1;
+            return 2;
+        }
+
+        private void Restart()
+        {
+            mutare = -1;
+            for (int i = 1; i <= 3; i++)
+                for (int j = 1; j <= 3; j++)
+                    mat[i, j] = 0;
+            foreach(Button b in this.Controls)
+            {
+                b.Text = "";
+            }
+        }
+
+        private void Computer_Move()
+        {
+            if (mutare == 10) return;
+            int configuratie = 0;
+            for (int i = 1; i <= 3; i++)
+                for (int j = 1; j <= 3; j++)
+                    if (mat[i, j] == 1)
+                        configuratie += pow3[3 * (i - 1) + j - 1];
+                    else if(mat[i, j] == 2)
+                        configuratie += 2 * pow3[3 * (i - 1) + j - 1];
+
+            int ii = 0, jj =  0, bestconf = 99999999;
+            for (int i = 1; i <= 3; i++)
+                for (int j = 1; j <= 3; j++)
+                    if (mat[i, j] == 0)
+                    {
+                        int newconf = configuratie + 2 * pow3[3 * (i - 1) + j - 1];
+                        if(C[newconf] < bestconf)
+                        {
+                            bestconf = C[newconf];
+                            ii = i; jj = j;
+                        }
+                    }
+            mat[ii, jj] = 2;
+
+            if (ii == 1 && jj == 1) button1.Text = "0"; else
+            if (ii == 1 && jj == 2) button2.Text = "0"; else
+            if (ii == 1 && jj == 3) button3.Text = "0"; else
+            if (ii == 2 && jj == 1) button4.Text = "0"; else
+            if (ii == 2 && jj == 2) button5.Text = "0"; else
+            if (ii == 2 && jj == 3) button6.Text = "0"; else
+            if (ii == 3 && jj == 1) button7.Text = "0"; else
+            if (ii == 3 && jj == 2) button8.Text = "0"; else
+            if (ii == 3 && jj == 3) button9.Text = "0";
+        }
+
+        private void checkGame()
+        {
+            Computer_Move();
+
+            line[1] = mat[1, 1] * mat[1, 2] * mat[1, 3];
+            line[2] = mat[2, 1] * mat[2, 2] * mat[2, 3];
+            line[3] = mat[3, 1] * mat[3, 2] * mat[3, 3];
+            line[4] = mat[1, 1] * mat[2, 1] * mat[3, 1];
+            line[5] = mat[1, 2] * mat[2, 2] * mat[3, 2];
+            line[6] = mat[1, 3] * mat[2, 3] * mat[3, 3];
+            line[7] = mat[1, 1] * mat[2, 2] * mat[3, 3];
+            line[8] = mat[3, 1] * mat[2, 2] * mat[1, 3];
+
+            for (int i = 1; i <= 8; i++)
+                if (line[i] == 1)
+                { MessageBox.Show("X castiga!"); Restart();  return; }
+                else if (line[i] == 8)
+                { MessageBox.Show("0 castiga!"); Restart(); return; }
+                else if (mutare == 9)
+                { MessageBox.Show("Egalitate!"); Restart(); return; }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (mat[1, 1] == 0)
+            {
+                mutare += 2;
+                mat[1, 1] = Jucator(mutare);
+                if (mat[1, 1] == 1)
+                    button1.Text = "X";
+                else
+                    button1.Text = "0";
+                checkGame();
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (mat[1, 2] == 0)
+            {
+                mutare += 2;
+                mat[1, 2] = Jucator(mutare);
+                if (mat[1, 2] == 1)
+                    button2.Text = "X";
+                else
+                    button2.Text = "0";
+                checkGame();
+            }
+
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            if (mat[1, 3] == 0)
+            {
+                mutare += 2;
+                mat[1, 3] = Jucator(mutare);
+                if (mat[1, 3] == 1)
+                    button3.Text = "X";
+                else
+                    button3.Text = "0";
+
+                checkGame();
+            }
+
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            if (mat[2, 1] == 0)
+            {
+                mutare += 2;
+                mat[2, 1] = Jucator(mutare);
+                if (mat[2, 1] == 1)
+                    button4.Text = "X";
+                else
+                    button4.Text = "0";
+                checkGame();
+            }
+
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            if (mat[2, 2] == 0)
+            {
+                mutare += 2;
+                mat[2, 2] = Jucator(mutare);
+                if (mat[2, 2] == 1)
+                    button5.Text = "X";
+                else
+                    button5.Text = "0";
+                checkGame();
+            }
+        }
+
+        private void button6_Click_1(object sender, EventArgs e)
+        {
+            if (mat[2, 3] == 0)
+            {
+                mutare += 2;
+                mat[2, 3] = Jucator(mutare);
+                if (mat[2, 3] == 1)
+                    button6.Text = "X";
+                else
+                    button6.Text = "0";
+                checkGame();
+            }
+        }
+
+        private void button7_Click_1(object sender, EventArgs e)
+        {
+            if (mat[3, 1] == 0)
+            {
+                mutare += 2;
+                mat[3, 1] = Jucator(mutare);
+                if (mat[3, 1] == 1)
+                    button7.Text = "X";
+                else
+                    button7.Text = "0";
+                checkGame();
+            }
+        }
+
+        private void button8_Click_1(object sender, EventArgs e)
+        {
+            if (mat[3, 2] == 0)
+            {
+                mutare += 2;
+                mat[3, 2] = Jucator(mutare);
+                if (mat[3, 2] == 1)
+                    button8.Text = "X";
+                else
+                    button8.Text = "0";
+                checkGame();
+            }
+        }
+
+        private void button9_Click_1(object sender, EventArgs e)
+        {
+            if (mat[3, 3] == 0)
+            {
+                mutare += 2;
+                mat[3, 3] = Jucator(mutare);
+                if (mat[3, 3] == 1)
+                    button9.Text = "X";
+                else
+                    button9.Text = "0";
+                checkGame();
+            }
+        }
+    }
+}
